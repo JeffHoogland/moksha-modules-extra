37509e9219f396cc436959dfeb6d53450883ca0b
Jeff Hoogland <jeffhoogland@linux.com>
2016-10-26 07:31:30 -0500

    Removed engage debian directory

--- engage/debian/engage.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the engage package
-#
-0 4	* * *	root	[ -x /usr/bin/engage_maintenance ] && /usr/bin/engage_maintenance
--- engage/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(engage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="engage" command="/usr/bin/engage"
