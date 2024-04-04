9f1aa189e0acd72cf9db6710d293d7cb29e914a1
Robert Wiley <ylee@bodhilinux.com>
2024-04-04 15:45:40 -0400

    add more-mods script

---
+++ more-mods.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git clone https://github.com/thewaiter/comptonmod
+git clone https://github.com/BodhiDev//Desktitle2.0
+git clone https://git.enlightenment.org/enlightenment/enlightenment-module-extramenu
+git clone https://github.com/BodhiDev/Forecast2.0
+git clone https://github.com/thewaiter/launcher-dictionary
+git clone https://github.com/rbtylee/launcher-spellchecker
+git clone https://github.com/rbtylee/emodule-productivity
+git clone https://github.com/thewaiter/StickyNotes
+git clone https://github.com/thewaiter/wincontrol
