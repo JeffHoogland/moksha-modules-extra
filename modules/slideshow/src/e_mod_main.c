a50db07b0dc3c9a3a0106547502dd53af33b8ef7
Robert Wiley <ylee@bodhilinux.com>
2024-12-28 16:52:26 -0500

    Adjust locale code

--- modules/slideshow/src/e_mod_main.c
+++ modules/slideshow/src/e_mod_main.c
@@ -337,11 +337,9 @@ EAPI E_Module_Api e_modapi =
 EAPI void *
 e_modapi_init(E_Module *m)
 {
-   char buf[PATH_MAX];
-
-   snprintf(buf, sizeof (buf), "%s/.e/e/backgrounds", e_user_homedir_get());
-   bindtextdomain(PACKAGE, buf);
-   bind_textdomain_codeset(PACKAGE, "UTF-8");
+   /* Set up module locales*/
+   bindtextdomain(LOCALEDOMAIN, LOCALEDIR);
+   bind_textdomain_codeset(LOCALEDOMAIN, "UTF-8");
 
    conf_item_edd = E_CONFIG_DD_NEW("Slideshow_Config_Item", Config_Item);
 #undef T
