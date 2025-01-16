111e55a447fe78093fb31d574c5059e1e0ae7001
Robert Wiley <ylee@bodhilinux.com>
2025-01-16 07:17:22 -0500

    Net: meson build added

--- modules/net/src/e_mod_main.c
+++ modules/net/src/e_mod_main.c
@@ -12,11 +12,9 @@ EAPI E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Net" };
 EAPI void *
 e_modapi_init(E_Module *m) 
 {
-   char buf[PATH_MAX];
-
-   snprintf(buf, sizeof(buf), "%s/locale", e_module_dir_get(m));
-   bindtextdomain(PACKAGE, buf);
-   bind_textdomain_codeset(PACKAGE, "UTF-8");
+   /* Set up module locales*/
+   bindtextdomain(LOCALEDOMAIN, LOCALEDIR);
+   bind_textdomain_codeset(LOCALEDOMAIN, "UTF-8");
 
    item_edd = E_CONFIG_DD_NEW("Config_Item", Config_Item);
    E_CONFIG_VAL(item_edd, Config_Item, id, STR);
