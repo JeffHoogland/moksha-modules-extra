d6ba6886c319773b4fa40f9381cd482a7c54d11b
Robert Wiley <ylee@bodhilinux.com>
2025-01-25 06:30:34 -0500

    News: fix local issue

--- modules/news/src/e_mod_main.c
+++ modules/news/src/e_mod_main.c
@@ -42,9 +42,9 @@ e_modapi_init(E_Module *m)
 {
    char buf[4096];
 
-   snprintf(buf, sizeof(buf), "%s/locale", e_module_dir_get(m));
-   bindtextdomain(PACKAGE, buf);
-   bind_textdomain_codeset(PACKAGE, "UTF-8");
+   /* Set up module locales*/
+   bindtextdomain(LOCALEDOMAIN, LOCALEDIR);
+   bind_textdomain_codeset(LOCALEDOMAIN, "UTF-8");
 
    news = E_NEW(News, 1);
    news->module = m;
