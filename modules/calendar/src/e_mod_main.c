ad7d6749aa3e1682b77080f503d7403ee26e3b80
Robert Wiley <ylee@bodhilinux.com>
2025-01-26 11:37:07 -0500

    Calendar: meson build added

--- modules/calendar/src/e_mod_main.c
+++ modules/calendar/src/e_mod_main.c
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
-
 #include <e.h>
 #include "e_mod_main.h"
 
@@ -164,7 +160,7 @@ static Evas_Object *
 _gc_icon(const E_Gadcon_Client_Class *client_class __UNUSED__, Evas *evas)
 {
    Evas_Object *o;
-   char buf[PATH_MAX];
+   char buf[4096];
 
    if (!calendar_conf->module) return NULL;
    snprintf(buf, sizeof(buf), "%s/e-module-calendar.edj",
@@ -668,10 +664,9 @@ EAPI E_Module_Api e_modapi =
 EAPI void *
 e_modapi_init(E_Module *m) 
 {
-   char buf[PATH_MAX];
-   snprintf(buf, sizeof(buf), "%s/locale", e_module_dir_get(m));
-   bindtextdomain(PACKAGE, buf);
-   bind_textdomain_codeset(PACKAGE, "UTF-8");
+   /* Set up module locales*/
+   bindtextdomain(LOCALEDOMAIN, LOCALEDIR);
+   bind_textdomain_codeset(LOCALEDOMAIN, "UTF-8");
 
    conf_item_edd = E_CONFIG_DD_NEW("Calendar_Config_Item", Config_Item);
    conf_edd = E_CONFIG_DD_NEW("Calendar_Config", Config);
