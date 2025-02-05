5c161ad985d5f05e648cfd30f91fc56dfb7d39e7
Robert Wiley <ylee@bodhilinux.com>
2025-02-05 06:59:40 -0500

    Update more-mods script

--- more-mods.sh
+++ more-mods.sh
@@ -2,10 +2,15 @@
 
 git clone https://github.com/thewaiter/comptonmod
 git clone https://github.com/BodhiDev//Desktitle2.0
+git clone https://github.com/rbtylee/moksha-module-drawer
 git clone https://git.enlightenment.org/enlightenment/enlightenment-module-extramenu
 git clone https://github.com/BodhiDev/Forecast2.0
 git clone https://github.com/thewaiter/launcher-dictionary
 git clone https://github.com/rbtylee/launcher-spellchecker
+# Private repo, uncomment if you have access and need
+# git clone https://github.com/thewaiter/Launcher-Places
+git clone https://github.com/rbtylee/moksha-moon-module
 git clone https://github.com/rbtylee/emodule-productivity
+git clone https://github.com/thewaiter/separator
 git clone https://github.com/thewaiter/StickyNotes
 git clone https://github.com/thewaiter/wincontrol
